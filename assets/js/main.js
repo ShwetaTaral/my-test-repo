70e68feb99bbf9f7b0b937dbcda675c8daf2d491
shwetataral <shwetataral29@gmail.com>
2014-03-09 10:45:30 +0530

    insert comment and change the naming conventions

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,38 @@
 // JavaScript Document
-var noofeachlinktext=[4,3,2,3];
-var trackeachtext=[0,0,0,0];
-var count1=1;
-var count2=0;
+var countLinktext=[4,3,2,3,1];  //maintain the count of each link's text in array 
+var linkTextArr=[0,0,0,0,0];	//tracking the each link text. Use for comparing with countLinktext
+var linkNode=1;  				//track the count of link.
+var linkNodeText=0;				//track the count of text in each link. 
 $(document).ready(function() 
 {
-	hidealltext();
-    $(".gotoNext").on("mousedown",function()
-	{
-		nextbuttonclick();
-    });
+	hidealltext();				//hide all the link text.
+   	gotonext();					//fucntion hanldes the next button click event.
 });
+
+function gotonext()
+{
+	 $(".btnNext").on("mousedown",function()		
+	{
+		nextbuttonclick();				//function for handling the btnNext click
+    });
+}
 
 function nextbuttonclick()
 {
 	
-	if(count1<=noofeachlinktext.length)
+	if(linkNode<=countLinktext.length)			
 	{
 		console.log("inside if")
-		if(trackeachtext[count1-1]!=noofeachlinktext[count1-1])
+		if(linkTextArr[linkNode-1]!=countLinktext[linkNode-1])
 		{
-			test();
+			displayText();				//Upadate the count value and display respective text.
 		}
-		else
+		else							//execute after each link text complete and move to next link text
 		{ 
 				console.log("inside else")
-					count1++;
-					count2=0;
-				test();
+					linkNode++;							
+					linkNodeText=0;
+					displayText();
 			
 		}
 			
@@ -35,40 +40,35 @@ function nextbuttonclick()
 }
 
 
-function test()
+function displayText()
 {
 	
-	hidealltext();
-	count2++;
-	
-	if(count1>noofeachlinktext.length)
+	hidealltext();			//hide all the initially displayed text
+	linkNodeText++;	
+	if(linkNode>countLinktext.length)     //if the value exceeds than the lenght of countLinktext
 	{
-		console.log("count1++"+count1);
-			count1=1;
-			count2=0;
-				for(i=0;i<trackeachtext.length;i++)
+			linkNode=1;								//reset the linkNode and linkNodeText
+			linkNodeText=1;
+				for(i=0;i<linkTextArr.length;i++)
 				{
-					trackeachtext[i]-=noofeachlinktext[i];
+					linkTextArr[i]-=countLinktext[i];		//reset the linkTextArr
 				}
 				
 	}
-	trackeachtext[count1-1]=count2;
-	$("#link"+count1+"text"+count2).show();	
-	
+	linkTextArr[linkNode-1]=linkNodeText;			//updating value of the linkTextArr
+	$("#link"+linkNode+"text"+linkNodeText).show();			//display the respective text
+	console.log("linkNode*"+ linkNode +"linkNodeText*"+ linkNodeText);
 }
 
 
 function hidealltext()
 {
-	for(var i=1;i<=noofeachlinktext.length;i++)
+	for(var i=1;i<=countLinktext.length;i++)
 	{
-		for(var j=1;j<=noofeachlinktext.length;j++)
+		for(var j=1;j<=countLinktext.length;j++)
 		{
-			$("#link"+i+"text"+j).hide();
+			$("#link"+i+"text"+j).hide();			//hide all the link text
 		}
 	}
-	
-/*$("#link1text1,#link1text2,#link1text3,#link1text4,#link2text1,#link2text2,#link2text3,#link2text4,#link3text1,#link3text2,#link3text3,#link3text4,#link4text1,#link4text2,#link4text3,#link4text4").hide();
-	*/
 }
 
